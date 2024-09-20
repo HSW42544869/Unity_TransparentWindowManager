cac602684ed2ba7965b69230be5e72c512e9f5ce
HSW42544869 <73380363+HSW42544869@users.noreply.github.com>
2024-09-20 17:52:47 +0800

    透明化面UI

---
+++ Assets/Packages/TransparentWindowManager/MinimizeWindowOnStart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class MinimizeWindowOnStart : MonoBehaviour
+{
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetActiveWindow();
+
+    [DllImport("user32.dll")]
+    private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+    private const int SW_MINIMIZE = 6;
+
+    void Start()
+    {
+        IntPtr windowHandle = GetActiveWindow();
+        ShowWindow(windowHandle, SW_MINIMIZE);
+    }
+}
--- Assets/Packages/TransparentWindowManager/TransparentWindowManager.cs
+++ Assets/Packages/TransparentWindowManager/TransparentWindowManager.cs
@@ -1,52 +1,11 @@
-﻿using System;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
 using System.Runtime.InteropServices;
 
 public class TransparentWindowManager : SingletonMonoBehaviour<TransparentWindowManager>
 {
-    #region Enum
-
-    internal enum WindowCompositionAttribute
-    {
-        WCA_ACCENT_POLICY = 19
-    }
-
-    internal enum AccentState
-    {
-        ACCENT_DISABLED = 0,
-        ACCENT_ENABLE_GRADIENT = 1,
-        ACCENT_ENABLE_TRANSPARENTGRADIENT = 2,
-        ACCENT_ENABLE_BLURBEHIND = 3,
-        ACCENT_INVALID_STATE = 4
-    }
-
-    #endregion Enum
-
-    #region Struct
-
-    private struct MARGINS
-    {
-        public int cxLeftWidth;
-        public int cxRightWidth;
-        public int cyTopHeight;
-        public int cyBottomHeight;
-    }
-
-    internal struct WindowCompositionAttributeData
-    {
-        public WindowCompositionAttribute Attribute;
-        public IntPtr Data;
-        public int SizeOfData;
-    }
-
-    internal struct AccentPolicy
-    {
-        public AccentState AccentState;
-        public int AccentFlags;
-        public int GradientColor;
-        public int AnimationId;
-    }
-
-    #endregion Struct
+    public Image interactiveArea; // 引用 UI Image 组件
 
     #region DLL Import
 
@@ -56,56 +15,120 @@ public class TransparentWindowManager : SingletonMonoBehaviour<TransparentWindow
     [DllImport("user32.dll")]
     private static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
 
+    [DllImport("user32.dll")]
+    private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
     [DllImport("Dwmapi.dll")]
     private static extern uint DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS margins);
 
+    [DllImport("user32.dll")]
+    private static extern bool GetCursorPos(out POINT lpPoint);
+
+    [DllImport("user32.dll")]
+    private static extern bool ScreenToClient(IntPtr hWnd, ref POINT lpPoint);
+
+    [DllImport("user32.dll")]
+    private static extern int GetSystemMetrics(int nIndex);
+
     #endregion DLL Import
 
-    #region Method
+    #region Structs and Constants
 
-    // CAUTION:
-    // To control enable or disable, use Start method instead of Awake.
+    private struct MARGINS
+    {
+        public int cxLeftWidth;
+        public int cxRightWidth;
+        public int cyTopHeight;
+        public int cyBottomHeight;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT
+    {
+        public int X;
+        public int Y;
+    }
+
+    private const int GWL_STYLE = -16;
+    private const int GWL_EXSTYLE = -20;
+    private const uint WS_POPUP = 0x80000000;
+    private const uint WS_VISIBLE = 0x10000000;
+    private const uint WS_EX_LAYERED = 0x00080000;
+    private const uint WS_EX_TRANSPARENT = 0x00000020;
+    private const uint WS_EX_TOPMOST = 0x00000008;
+
+    private const int SM_CXSCREEN = 0;
+    private const int SM_CYSCREEN = 1;
+
+    private const uint SWP_SHOWWINDOW = 0x0040;
+
+    #endregion
+
+    private IntPtr windowHandle;
 
     protected virtual void Start()
     {
         #if !UNITY_EDITOR && UNITY_STANDALONE_WIN
-
-        const int  GWL_STYLE = -16;
-        const uint WS_POPUP = 0x80000000;
-        const uint WS_VISIBLE = 0x10000000;
-
-        // NOTE:
-        // https://msdn.microsoft.com/ja-jp/library/cc410861.aspx
-
-        var windowHandle = GetActiveWindow();
-
-        // NOTE:
-        // https://msdn.microsoft.com/ja-jp/library/cc411203.aspx
-        // 
-        // "SetWindowLong" is used to update window parameter.
-        // The arguments shows (target, parameter, value).
-
-        SetWindowLong(windowHandle, GWL_STYLE, WS_POPUP | WS_VISIBLE);
-
-        // NOTE:
-        // https://msdn.microsoft.com/ja-jp/library/windows/desktop/aa969512(v=vs.85).aspx
-        // https://msdn.microsoft.com/ja-jp/library/windows/desktop/bb773244(v=vs.85).aspx
-        // 
-        // DwmExtendFrameIntoClientArea will spread the effects
-        // which attached to window frame to contents area.
-        // So if the frame is transparent, the contents area gets also transparent.
-        // MARGINS is structure to set the spread range.
-        // When set -1 to MARGIN, it means spread range is all of the contents area.
-
-        MARGINS margins = new MARGINS()
-        {
-            cxLeftWidth = -1
-        };
-
-        DwmExtendFrameIntoClientArea(windowHandle, ref margins);
-
-        #endif // !UNITY_EDITOR && UNITY_STANDALONE_WIN
+        MakeWindowTransparentAndFullscreen();
+        #endif
     }
 
-    #endregion Method
+    void Update()
+    {
+        #if !UNITY_EDITOR && UNITY_STANDALONE_WIN
+        UpdateInteractiveArea();
+        #endif
+    }
+
+    private void MakeWindowTransparentAndFullscreen()
+    {
+        windowHandle = GetActiveWindow();
+
+        // 获取屏幕尺寸
+        int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+        int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+        // 设置窗口样式
+        SetWindowLong(windowHandle, GWL_STYLE, WS_POPUP | WS_VISIBLE);
+
+        // 设置扩展窗口样式：layered, transparent 和 topmost
+        SetWindowLong(windowHandle, GWL_EXSTYLE, WS_EX_LAYERED | WS_EX_TRANSPARENT | WS_EX_TOPMOST);
+
+        // 扩展窗口框架
+        MARGINS margins = new MARGINS() { cxLeftWidth = -1 };
+        DwmExtendFrameIntoClientArea(windowHandle, ref margins);
+
+        // 设置窗口位置和大小为全屏
+        SetWindowPos(windowHandle, new IntPtr(-1), 0, 0, screenWidth, screenHeight, SWP_SHOWWINDOW);
+    }
+
+    private void UpdateInteractiveArea()
+    {
+        if (interactiveArea == null) return;
+
+        POINT cursorPos;
+        GetCursorPos(out cursorPos);
+        ScreenToClient(windowHandle, ref cursorPos);
+
+        Vector2 localPoint;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            interactiveArea.rectTransform, 
+            new Vector2(cursorPos.X, cursorPos.Y), 
+            null, out localPoint);
+
+        bool isInsideInteractiveArea = interactiveArea.rectTransform.rect.Contains(localPoint);
+
+        uint exStyle = (uint)SetWindowLong(windowHandle, GWL_EXSTYLE, 0);
+        if (isInsideInteractiveArea)
+        {
+            // 在交互区域内，移除 WS_EX_TRANSPARENT 标志
+            exStyle &= ~WS_EX_TRANSPARENT;
+        }
+        else
+        {
+            // 在交互区域外，添加 WS_EX_TRANSPARENT 标志
+            exStyle |= WS_EX_TRANSPARENT;
+        }
+        SetWindowLong(windowHandle, GWL_EXSTYLE, exStyle | WS_EX_LAYERED | WS_EX_TOPMOST);
+    }
 }
